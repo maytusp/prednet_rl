630e3dfd75bf4a648808814c39aca92e504a4b40
maytusp <maytusp@gmail.com>
2026-05-23 08:37:27 +0100

    runnable bash scripts and settings

--- scripts/run_ddpg_nonstationary.sh
+++ scripts/run_ddpg_nonstationary.sh
@@ -5,15 +5,13 @@
 #SBATCH -n 1                 # One Slurm task
 #SBATCH -c 12                # CPU cores available to the host code.
 
-set -euo pipefail
 
 cd ..
 SCRIPT_DIR="$(pwd)"
 echo "Script directory: ${SCRIPT_DIR}"
 
-CONDA_ENV="${CONDA_ENV:-simple_sfs}"
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate "${CONDA_ENV}"
+
+source activate habitat
 
 export MUJOCO_GL="${MUJOCO_GL:-egl}"
 
